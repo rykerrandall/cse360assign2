60056d244fee8deabfa385c0853d8c1a49ec6bf2
ryker <ryker@Rykers-HP-Laptop>
2019-02-22 20:51:29 -0700

    Finished Assignment Fixed

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -4,67 +4,91 @@ CSE 360 Assignment 2
 */
 
 package cse360assign2;
+
 /**
  * A file that is made to work as a calculator
  * @author ryker
  *
  */
+
 public class Calculator {
 
 	private int total;
 	private String history = "0";
+	
+	
 	/**
 	 * This sets the total to 0 originally
 	 */
+	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
+	
+	
 	/**
 	 * This function is used to get the total after all computation
 	 * @return the total
 	 */
+	
 	public int getTotal () {
 		return total;
 	}
+	
+	
 	/**
 	 * This function is to add a value to the total
 	 * @param value is the value to add
 	 */
+	
 	public void add (int value) {
 		total = total + value;
 		history += " + " + value;
 	}
+	
+	
 	/**
 	 * This function is to subtract a value from the total
 	 * @param value is the desired number to subtract
 	 */
+	
 	public void subtract (int value) {
 		total = total - value;
 		history += " - " + value;
 	}
+	
+	
 	/**
 	 * This function is to multiply the total by a number
 	 * @param value is the desired multiplier
 	 */
+	
 	public void multiply (int value) {
 		total = total * value;
 		history += " * " + value;
 	}
+	
+	
 	/**
 	 * This function is to divide the total by a number
 	 * @param value is the desired number by which the total will be divided
 	 */
+	
 	public void divide (int value) {
 		if(value == 0)
 			total = 0;
 		else
 			total = total / value;
+		
 		history += " / " + value;
 	}
+	
+	
 	/**
 	 * This function is to get the history of computations
 	 * @return returns the history that has occured
 	 */
+	
 	public String getHistory () {
 		return history;
 	}
